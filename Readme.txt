0c8c2e44cf9220062f11fc5a65323d15f33614c8
Oleg Mikulskiy <oleg.mikulskiy@eleks.com>
2017-07-13 18:32:14 +0300

    expjs another educational app

---
+++ Readme.txt
@@ -0,0 +1,52 @@
+1. Install NodeJS
+https://nodejs.org/en/download/
+
+2. install express
+npm install -g express
+npm install -g express generator
+
+3. Run Git Bush Here
+npm init
+
+npm install
+
+node app
+
+
+--Read about poug and other template engines
+
+--Express validator module using to validate input fields for example
+npm install express-validator --save
+
+
+--Express Validator
+https://github.com/ctavan/express-validator
+
+--MongoDB
+
+--mongoJS ORM
+https://github.com/mafintosh/mongojs
+
+
+// Sample object
+
+var users = [
+  {
+    id: 1,
+    first_name: 'John',
+    last_name: 'Doe',
+    email: 'johndoe@gmail.com'
+  },
+  {
+    id: 2,
+    first_name: 'Bob',
+    last_name: 'Smith',
+    email: 'bobsmith@gmail.com'
+  },
+  {
+    id: 3,
+    first_name: 'Jill',
+    last_name: 'Jackson',
+    email: 'jjackson@gmail.com'
+  }
+]
---
+++ app.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('test', ['users']);
+var app = express();
+
+//View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Body Parser Middlware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Global Variables
+app.use(function(req, res, next){
+  res.locals.errors = null;
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+// Set the Routes
+app.get('/',function(req, res){
+  db.users.find(function(err, docs) {
+      res.render('index', {
+       title: 'Education',
+       users: docs
+      });
+    })
+});
+
+// Do Action when Post Request Comes
+app.post('/users/add', function(req, res){
+
+// Input Field Validation
+  req.checkBody('first_name', 'First Name is Required').notEmpty();
+  req.checkBody('last_name', 'Last Name is Required').notEmpty();
+  req.checkBody('email', 'Email is Required').notEmpty();
+
+  var errors = req.validationErrors();
+
+  if(errors){
+      res.render('index', {
+        title: 'Education',
+        users: users,
+        errors: errors
+      });
+  } else {
+    var newUser = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+    }
+    db.users.insert(newUser, function(err, result){
+        if(err){
+          console.log(err);
+        }
+        res.redirect('/');
+    });
+  }
+});
+
+app.listen(3000, function(){
+  console.log('Server started on port 3000...');
+})
